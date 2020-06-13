3ae3c62a03342a2a550ba4460083bbfa8e77bba7
darrenlefort <63559435+darrenlefort@users.noreply.github.com>
2020-06-13 16:39:53 -0400

    added csv pathway

---
+++ PyBank/main.py
@@ -0,0 +1,10 @@
+import csv
+import os
+
+pathway = os.path.join("..", "PyBank_data.csv")
+
+with open (pathway) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
+
+    csv_header = next(csvfile, None)
+    print(f"Header: {csv_header}")
