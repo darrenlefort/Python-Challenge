839235c7064b63379340a78e69dc78be398526ad
darrenlefort <63559435+darrenlefort@users.noreply.github.com>
2020-06-16 14:39:32 -0400

    format print and export file for multiple lines

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,6 +12,31 @@ with open (pathway) as csvfile:
 
 txt_header = "Financial Analysis"
 
+txt_lines = [
+    [txt_header], 
+    ["---------------------------"], 
+    ["Total Months: "], 
+    ["Total: $"], 
+    ["Average Change: $"], 
+    ["Greatest Increase in Profits: "], 
+    ["Greatest Decrease in Profits: "]]
+
+for s in txt_lines:
+    print(*s)
+
+
 output_path = open("analysis/PyBank_analysis.txt", "w")
-output_path.writelines(txt_header)
+output_path.write(txt_header)
+output_path.write("\n") 
+output_path.write("---------------------------")
+output_path.write("\n") 
+output_path.write("Total Months: ")
+output_path.write("\n") 
+output_path.write("Total: $")
+output_path.write("\n") 
+output_path.write("Average Change: $")
+output_path.write("\n") 
+output_path.write("Greatest Increase in Profits: ")
+output_path.write("\n")  
+output_path.write("Greatest Decrease in Profits: ")
 output_path.close()
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -11,7 +11,45 @@ with open (pathway) as csvfile:
 
     
 txt_header = "Election Results"
-    
+
+txt_lines = [
+    [txt_header], 
+    ["---------------------------"], 
+    ["Total Votes: "], 
+    ["---------------------------"], 
+    ["Winner Name: "], 
+    ["2nd Place Name: "], 
+    ["3rd Place Name: "], 
+    ["4th Place Name: "], 
+    ["---------------------------"], 
+    ["Winner Name: "], 
+    ["---------------------------"]
+]
+
+for s in txt_lines:
+    print(*s)
+   
 output_path = open("analysis/PyPoll_analysis.txt", "w")
-output_path.writelines(txt_header)
+output_path.write(txt_header)
+output_path.write("\n") 
+output_path.write("---------------------------")
+output_path.write("\n") 
+output_path.write("Total Votes: ")
+output_path.write("\n") 
+output_path.write("---------------------------")
+output_path.write("\n") 
+output_path.write("Winner Name: ")
+output_path.write("\n") 
+output_path.write("2nd Place Name: ")
+output_path.write("\n")  
+output_path.write("3rd Place Name: ")
+output_path.write("\n")  
+output_path.write("4th Place Name: ")
+output_path.write("\n")  
+output_path.write("---------------------------")
+output_path.write("\n") 
+output_path.write("Winner Name: ")
+output_path.write("\n")  
+output_path.write("---------------------------")
+output_path.write("\n") 
 output_path.close()
