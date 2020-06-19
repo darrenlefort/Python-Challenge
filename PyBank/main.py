d522f41e6a4b984dc732db5417d2554c180585a3
darrenlefort <63559435+darrenlefort@users.noreply.github.com>
2020-06-18 23:04:45 -0400

    PyBank Calculations added

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,23 +3,47 @@ import os
 
 pathway = os.path.join("Resources", "PyBank_data.csv")
 
+dates = []
+profit = []
+loss = []
+
 with open (pathway) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
 
     csv_header = next(csvfile, None)
     print(f"Header: {csv_header}")
 
+    sum_profit = 0.0
+    for row in csvreader:
+        dates.append(str(row[0]))
+        sum_profit = row[1]
+        if int(row[1]) > 0:
+            profit.append(int(row[1]))
+        else:
+            loss.append(int(row[1]))
+    
+        
+
+total_months = str(len(dates))
+average_change = str(int(sum_profit) / int(total_months))
+max_increase = max(profit)
+max_loss = min(loss)
+print(total_months)
+print(sum_profit)
+print(average_change)
+print(max_increase)
+print(max_loss)
 
 txt_header = "Financial Analysis"
-
+total_months = str(total_months)
 txt_lines = [
     [txt_header], 
     ["---------------------------"], 
-    ["Total Months: "], 
-    ["Total: $"], 
-    ["Average Change: $"], 
-    ["Greatest Increase in Profits: "], 
-    ["Greatest Decrease in Profits: "]]
+    ["Total Months: " + total_months], 
+    ["Total: $" + sum_profit], 
+    ["Average Change: $" + average_change], 
+    ["Greatest Increase in Profits: " + str(max_increase)], 
+    ["Greatest Decrease in Profits: " + str(max_loss)]]
 
 for s in txt_lines:
     print(*s)
